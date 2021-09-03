356c9128e0d6975e651852fccf7873017014489c
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-09-03 16:50:29 +0530

    creating custom Navbar component using props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,10 @@
 
 import './App.css';
 
+// import the Navbar component to this App.js file 
+import Navbar from './components/Navbar';
+
+
 /* 
   Yeh joh html lika hua hai inside the return (); of App.js, yeh actually mein JSX hai.
 
@@ -14,32 +18,11 @@ function App() {
     <div className="App">
 
       {/* Navbar Component of our react application */}
+        
+        {/* Serve/Use the custom NavBar Component here */}
+        {/* We will use the Navbar component by writing <Navbar/> inside return(). */}
+        <Navbar/> 
 
-      {/* Added Bootstrap Component ==> Navbar */}
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">TextUtils</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">About</a>
-              </li>
-
-            </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
       
     </div>
   );
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,45 @@
+// Creating a custom Navbar Component
+
+// We are creating a custom function based component of react
+
+import React from "react"
+
+export default function Navbar() {
+
+    return (
+        <div>
+
+            {/* Here will be the code of Navbar component */}
+            
+            {/* Added Bootstrap Component ==> Navbar */}
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                <a className="navbar-brand" href="/">TextUtils</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">About</a>
+                    </li>
+
+                    </ul>
+                    <form className="d-flex">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+                </div>
+            </nav>
+
+        </div>
+    )
+}
+
+// What is the next step?
+// ANS: Now we have created our navbar, but we have to import it to App.js(the file inside the src folder) to serve it.
