11d6bb4a0de0f10968f5607f8dc1cbf31712c170
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-09-03 19:09:27 +0530

    Hooks - useState (Steps to use useState)

--- my-app/src/components/Textform.js
+++ my-app/src/components/Textform.js
@@ -3,9 +3,48 @@
 
 // We are creating a custom function based component of react
 
-import React from 'react'
+// import React from 'react'
+
+
+// useState / State Hook in react:
+/*
+
+Steps to use useState:
+    Step-1: Firstly import the state to your react app
+    
+    Step-2: To use state, firstly enter the following code inside your function-based component.
+                // Declare a new state variable, which we'll call "count"
+                const [count, setCount] = useState(0);
+    
+    Step-3: The count is the state variable, which contains the current state value. In our case it is text. 
+            So, we will replace count with text.
+                const [text, setText] = useState("Enter Text here");
+
+    Step-4: Here, The value of our text is ‘Enter text here’ . 
+            Let's pass this value in our textbox and make sure to use the ‘Onchange’ event to enable text-entry in your textbox. 
+            
+            To do so change the code of your text area to:
+            <textarea className= "form-Control value={text} Onchange={handleOnchange} id="mybox" rows= "8"></textarea>
+
+
+*/
+
+// Step-1: Firstly import the state to your react app
+import React, { useState } from 'react'
+
+
+
                             // using Props
 export default function Textform(props) {
+
+
+    //Step-2: To use state, firstly enter the following code inside your function-based component.
+    
+    // Declare a new state variable, which we'll call "text"
+    const [text, setText] = useState("Enter Text Here!");
+
+
+
     return (
         <div>
             {/* Heading */}
@@ -15,7 +54,7 @@ export default function Textform(props) {
             {/* https://getbootstrap.com/docs/5.1/forms/form-control/ */}
             <div className="mb-3">
                 {/* Textbox for our app */}
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea className="form-control" value={text} id="myBox" rows="8"></textarea>
             </div>
 
             {/* Adding Button */}
